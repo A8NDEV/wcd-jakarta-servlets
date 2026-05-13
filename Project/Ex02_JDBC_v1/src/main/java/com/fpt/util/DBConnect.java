a835ae95d3d6e74b19e6f4f361b6a1057a21d2ab
ANDEV09 <lamhoangan612@gmail.com>
2026-05-13 20:37:30 +0700

    feat: implement database connection utility for MySQL JDBC integration

--- Project/Ex02_JDBC_v1/src/main/java/com/fpt/util/DBConnect.java
+++ Project/Ex02_JDBC_v1/src/main/java/com/fpt/util/DBConnect.java
@@ -8,7 +8,7 @@ public class DBConnect {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wcdlap", "root", "");
+            cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wcdlab", "root", "");
         } catch (Exception e) {
             e.printStackTrace();
         }
