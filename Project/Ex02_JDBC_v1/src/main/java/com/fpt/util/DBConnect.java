86137eaa997d9b8fb3280d9231515b9f653f181a
ANDEV09 <lamhoangan612@gmail.com>
2026-05-13 20:33:28 +0700

    feat: add database connection utility and initialize NetBeans project private configuration

--- Project/Ex02_JDBC_v1/src/main/java/com/fpt/util/DBConnect.java
+++ Project/Ex02_JDBC_v1/src/main/java/com/fpt/util/DBConnect.java
@@ -1,5 +1,4 @@
 package com.fpt.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -9,7 +8,7 @@ public class DBConnect {
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/lab", "root", "12345678");
+            cnn = DriverManager.getConnection("jdbc:mysql://localhost:3306/wcdlap", "root", "");
         } catch (Exception e) {
             e.printStackTrace();
         }
