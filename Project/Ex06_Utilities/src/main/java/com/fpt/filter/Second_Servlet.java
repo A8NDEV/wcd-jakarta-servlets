dacd03d08e241fd24eb88550553f5a39e4054f65
ANDEV09 <lamhoangan612@gmail.com>
2026-05-22 21:18:50 +0700

    feat: implement Filter demo with First_Filter, Second_Servlet, and rs01_Filter.jsp

---
+++ Project/Ex06_Utilities/src/main/java/com/fpt/filter/Second_Servlet.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.fpt.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS
+ */
+@WebServlet(name = "Second_Servlet", urlPatterns = { "/Second_Servlet" })
+public class Second_Servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Second_Servlet</title>");
+            out.println("<script src=\"https://cdn.tailwindcss.com\"></script>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Second_Servlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+
+            // 1. Đọc kết quả không qua filter
+            String withoutFilter;
+            withoutFilter = request.getParameter("txtName");
+            // 2. Đọc kết quả qua First_Filter
+            String withFilter;
+            withFilter = (String) request.getAttribute("test_name");
+            // 3. Hiển thị
+            out.println("<h3>Tên không dùng Filter<span class='text-blue-500 font-bold'> " + withoutFilter + "</span></h3>");
+
+            out.println("<h3>Tên không dùng Filter<span class='text-red-500 font-bold'> " + withFilter + "</span></h3>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
