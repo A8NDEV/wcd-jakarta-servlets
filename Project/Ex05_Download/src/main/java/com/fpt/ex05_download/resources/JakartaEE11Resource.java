d2eb5339e579ea44de307d83a2c5a7a7b98c084a
ANDEV09 <lamhoangan612@gmail.com>
2026-05-27 20:47:05 +0700

    feat: add Ex05_Download project

---
+++ Project/Ex05_Download/src/main/java/com/fpt/ex05_download/resources/JakartaEE11Resource.java
@@ -0,0 +1,20 @@
+package com.fpt.ex05_download.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class JakartaEE11Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
